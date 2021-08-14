cd650bb9d37f28dcdb287e3714b4f24e8b7be150
LuauProgrammer <aydentaylorl@gmail.com>
2021-08-14 14:50:55 -0400

    added package.lua

---
+++ package.lua
@@ -0,0 +1,18 @@
+  return {
+    name = "LuauProgrammer/Bloxed",
+    version = "0.0.1",
+    description = "ROBLOX API Wrapper for the Luvit Runtime Enviroment.",
+    tags = { "roblox", "api", "wrapper" },
+    license = "MIT",
+    author = { name = "LuauProgrammer", email = "aydentaylorl@gmail.com" },
+    homepage = "https://github.com/LuauProgrammer/Bloxed",
+    dependencies = {
+      'creationix/coro-http',
+      'luvit/secure-socket',
+      'luvit/json',
+    },
+    files = {
+      "**.lua"
+    }
+  }
+  
