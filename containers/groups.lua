96000499e0fb1486683c71bb3b9b2fb26c2c7071
LuauProgrammer <aydentaylorl@gmail.com>
2021-08-14 19:52:50 -0400

    removed a line

--- containers/groups.lua
+++ containers/groups.lua
@@ -13,9 +13,9 @@ local json = require('json')
 module.HandleJoinRequest = function(userID,groupID)
     local res, body = http.request("POST", "https://groups.roblox.com/v1/groups/"..groupID.."/join-requests/users/"..userID, {{"Content-Type", "text/json"},{"Content-Length", "0"}, {"X-CSRF-TOKEN", X_CSRF_TOKEN}, {"Cookie", token}})
     if res.code ~= 200 then
-        return nil
+        return false
     end
-    return json.decode(body)
+    return true
 end
 
 module.GetGroupInformation = function(groupID)
